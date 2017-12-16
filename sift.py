99f84b57b8b412071c14ca93b22cbce734dc1a2f
jylhaust <jylhaust@163.com>
2017-12-16 14:48:51 +0800

    win10

--- sift.py
+++ sift.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 # from cv2.cv import *
 import cv2
-# import numpy as np
-print('hello') 
-# img = cv2.imread('C:\Users\yuanlin\Pictures\pic 1.jpg')
+import numpy as np
+# print('hello') 
+img = cv2.imread('1.jpg',cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+# print(img)
+sift = cv2.xfeatures2d.SIFT_create()
+# keypoints = sift.detect(gray,None)
+# img = cv2.drawKeypoints(gray,keypoints)
+# cv2.imshow('test',img)
+
+# cv2.waitKey(0)
+# cv2.destroyAllwindows()
