a38b275090b4715e69d8559f4e96ce38f66269fe
jylhaust <jylhaust@163.com>
2018-01-10 19:52:55 +0800

    update

--- VO/build/CMakeFiles/pose2d2d.dir/DependInfo.cmake
+++ VO/build/CMakeFiles/pose2d2d.dir/DependInfo.cmake
@@ -8,35 +8,15 @@ set(CMAKE_DEPENDS_CHECK_CXX
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/home/yuanlin/opencv-2.4.13.4/build"
-  "/home/yuanlin/opencv-2.4.13.4/include"
-  "/home/yuanlin/opencv-2.4.13.4/include/opencv"
-  "/home/yuanlin/opencv-2.4.13.4/modules/core/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/flann/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/imgproc/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/highgui/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/features2d/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/calib3d/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/ml/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/video/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/legacy/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/objdetect/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/photo/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/gpu/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/ocl/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/nonfree/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/contrib/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/stitching/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/superres/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/ts/include"
-  "/home/yuanlin/opencv-2.4.13.4/modules/videostab/include"
-  )
-
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- VO/poseEstimate2d2d.cpp
+++ VO/poseEstimate2d2d.cpp
@@ -28,35 +28,46 @@ int main( int argc, char** argv )
        cout<<"img not find"<<endl;
         // return 1;
    }
-   cout<<"argc:"<<" "<<argc<<endl;
+    cout<<"argc:"<<" "<<argc<<endl;
 
-    cv::Mat img1 = cv::imread( argv[1], CV_LOAD_IMAGE_COLOR );///home/yuanlin/SLAM-Porj/VO/6.jpg
+    cv::Mat img1 = cv::imread( "/home/yuanlin/SLAM-Porj/VO/6.jpg", CV_LOAD_IMAGE_COLOR );///home/yuanlin/SLAM-Porj/VO/6.jpg
     cv::Mat img2 = cv::imread( argv[2], CV_LOAD_IMAGE_COLOR );
-
+/* */ 
     vector<KeyPoint> keypoint1, keypoint2;
     vector<DMatch> matches;
-    // find_feature_matches (img1, img2, keypoint1, keypoint2, matches);
-/* */
+    find_feature_matches (img1, img2, keypoint1, keypoint2, matches);
+ 
     Mat R,t;
-    // pose_estimation_2d2d( keypoint1, keypoint2, matches, R, t );
+    pose_estimation_2d2d( keypoint1, keypoint2, matches, R, t );
 
     Mat tx = ( Mat_<double> (3,3) << 
-                0,  -t.at<double> (2,0), t.at<double> (1,0), 
-                t.at<double> (2,0), 0,   -t.at<double> (0,0)         
+                0,  -t.at<double> (2,0),  t.at<double> (1,0), 
+                t.at<double> (2,0), 0,   -t.at<double> (0,0),         
                 -t.at<double> (1,0), t.at<double> (0,0), 0 );
     cout<<"t^R:"<<tx*R<<endl;        
-     
-    cv::imshow( "origin",img1 );
-    cv::waitKey(0);
+   // 验证对极约束
+    // Mat K = ( Mat_<double> (3,3)　<< 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
+    Mat K = ( Mat_<double> ( 3,3 ) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
+
+   for ( DMatch m: matches )
+   {
+       Point2d pt1 = pixel2cam ( keypoint1[ m.queryIdx ].pt, K );
+       Mat y1 = ( Mat_<double>(3,1) << pt1.x, pt1.y, 1);
+       Point2d pt2 = pixel2cam( keypoint2[ m.trainIdx ].pt, K );
+       Mat y2 = ( Mat_<double>(3,1) << pt2.x, pt2.y, 2);
+       Mat d = y2.t() * tx * R *y1;
+   }
+    // cv::imshow( "origin",img1 );
+    // cv::waitKey(0);
     return 0;
 }
-
+/**/
 void pose_estimation_2d2d( 
     vector<KeyPoint> keypoint1,
     vector<KeyPoint> keypoint2,
     vector<DMatch> matches,
     Mat& R, Mat& t)
-{
+{ 
     //相机内参
     Mat K = ( Mat_<double> (3,3 ) << 520.9, 325.1, 0, 521.0, 249.7,0 ,0, 1);
 
@@ -64,45 +75,52 @@ void pose_estimation_2d2d(
     vector<Point2f> point2;
     for( int i = 0; i<( int ) matches.size(); i++)
     {
+        //query是要匹配的描述子，train是被匹配的描述子
         point1.push_back( keypoint1[matches[i].queryIdx].pt );
         point2.push_back( keypoint2[ matches[i].trainIdx].pt );
 
     }
     //基础矩阵计算
     Mat funda_mat;
-    // funda_mat = findFundamentalMat ( point1, point2, CV_FM_8POINT );
+    funda_mat = findFundamentalMat ( point1, point2, CV_FM_8POINT );
 
     //本质矩阵计算
-    // cv::Point2d principal_point (325.1, 249.7);
+    cv::Point2d principal_point (325.1, 249.7);
     int focal_length = 521; 
     Mat esst_mat;
-    // esst_mat = findEssentialMat( point1, point2, focal_length, principal_point, RANSAC );
+    esst_mat = findEssentialMat( point1, point2, focal_length, principal_point, RANSAC );
 
     //单应矩阵计算
     Mat homo_mat;
-    // homo_mat = findHomography( point1, point2, RANSAC, 3, noArray());
-
-    // recoverPose( esst_mat, point1, point2, R, t, focal_length, principal_point);
+    homo_mat = findHomography( point1, point2, RANSAC, 3, noArray(), 2000, 0.99);
+    //获取R,t
+    recoverPose( esst_mat, point1, point2, R, t, focal_length, principal_point );
 
 }
-/*
+/**/
 void find_feature_matches( const Mat& img1, const Mat& img2,
                         vector<KeyPoint>& keypoint1,
-                        vector<KeyPoint<& keypoint2,
+                        vector<KeyPoint>& keypoint2,
                         vector< DMatch >& matches )
-{at
+{
     Mat descriptor1, descriptor2;
-    // Ptr<FeatureDetector> detector = ORB::create();
-    // Ptr<DescriptorExtractor> descriptor = ORB::create();
-    Ptr<FeatureDetector> detector = FeatureDetector::create ( "ORB" );
-    Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ( "ORB" );
+    Ptr<FeatureDetector> detector = ORB::create();
+    Ptr<DescriptorExtractor> descriptor = ORB::create();
+    // Ptr<FeatureDetector> detector = FeatureDetector::create ( "ORB" );
+    // Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ( "ORB" );
 
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::creat( "BruteForce-Hamming");
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create( "BruteForce-Hamming");
+
+    //1. 检测fast角点位置
+    detector->detect (img1,keypoint1);
+    detector->detect (img2,keypoint2);
+    //2. 根据角点计算BRIEFF描述子
     detector->compute ( img1, keypoint1, descriptor1 );
     detector->compute ( img2, keypoint2, descriptor2 );
-
+    //3. 对图像中的描述子进行匹配
     vector<DMatch> match;
     matcher->match( descriptor1, descriptor2, match);
+    //4.　匹配点对筛选
     double min_dist = 10000, max_dist = 0;
 
     for( int i = 0;i < descriptor1.rows; i++)
@@ -113,11 +131,12 @@ void find_feature_matches( const Mat& img1, const Mat& img2,
     }
     for( int i = 0; i < descriptor1.rows; i++ )
     {
-        if( match[i].distance <= max ( 2*min_dist, 30 ))
+        if ( match[i].distance <= max ( 2*min_dist, 30.0 ) )
             matches.push_back( match[i] );
+            
     }
 }
-*/
+
 Point2d pixel2cam( const Point2d& p, const Mat& K )
 {
     return Point2d
